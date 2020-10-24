da3ab2c9c0983859e2f7d7e754d6a7d3909cbe45
cvespa <christian@algomus.com>
2020-10-24 11:11:00 -0400

    Updated social to handle for game icons

--- src/SocialMediaList.js
+++ src/SocialMediaList.js
@@ -6,25 +6,25 @@ class SocialMediaList extends React.Component {
     constructor(props) {
         super(props);
 
+        if (this.props.games !== undefined && this.props.games !== null && Array.isArray(this.props.games)) {
+            this.games = this.props.games
+        } else {
+            this.games = [];
+        }
+
         if (this.props.socials !== undefined && this.props.socials !== null && Array.isArray(this.props.socials)) {
             this.socials = this.props.socials
         } else {
             this.socials = [];
         }
+    }
 
-        if (this.props.instagram !== undefined && this.props.instagram !== null) {
-            this.socials.push({
-                link: "https://instagram.com/" + this.props.instagram,
-                icon: "instagram"
-            });
-        }
-
-        if (this.props.github !== undefined && this.props.github !== null) {
-            this.socials.push({
-                link: "https://github.com/" + this.props.github,
-                icon: "github"
-            });
-        }
+    renderGameIcon(name, href, skin) {
+        return (
+            <a href={href} target="_blank" rel="noopener noreferrer">
+                <img className="game" src={skin} alt={name} />
+            </a>
+        )
     }
 
     renderSocialMediaLink(href, icon) {
@@ -35,10 +35,22 @@ class SocialMediaList extends React.Component {
         )
     }
 
-    renderList(socials) {
+    renderList(games, socials) {
         let items = []
 
         // Outer loop to create items
+        if (games !== undefined && games !== null && Array.isArray(games)) {
+            for (let i = 0; i < games.length; i++) {
+                let game = games[i];
+                if (game !== undefined && game !== null) {
+                    //Create the item add the link
+                    items.push(<li key={i}>
+                        {this.renderGameIcon(game.name, game.link, game.skin)}
+                    </li>)
+                }
+            }
+        }
+
         if (socials !== undefined && socials !== null && Array.isArray(socials)) {
             for (let i = 0; i < socials.length; i++) {
                 let social = socials[i];
@@ -61,7 +73,7 @@ class SocialMediaList extends React.Component {
     render() {
         return (
             <div className="social-media-list">
-                {this.renderList(this.socials)}
+                {this.renderList(this.games, this.socials)}
             </div>
         );
     }
