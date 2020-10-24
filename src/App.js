9d49d98a1bb4d2bec856065760583268577051ce
cvespa <christian@algomus.com>
2020-10-24 11:11:31 -0400

    Locked everything in to place

--- src/App.js
+++ src/App.js
@@ -3,20 +3,35 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import SocialMediaList from './SocialMediaList.js'
 import './App.css';
+import IGNList from './IGNList.js';
 
 library.add(fab);
+
+let igns = ["Battle Programmer Otacon", "bpotacon", "cvespa"]
+let games = [{
+  name: "Minecraft",
+  link: "https://www.minecraft.net/en-us/store/minecraft-java-edition",
+  skin: "https://crafatar.com/avatars/b5714800-1c21-40e9-8b5c-161e69b6cffc?overlay=true"
+}];
+let socials = [{
+  link: "https://instagram.com/bpotacon",
+  icon: "instagram"
+}, {
+  link: "https://github.com/cvespa",
+  icon: "github"
+}];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Battle Programmer Otacon</p>
+        <IGNList igns={igns}/>
       </header>
       <div className="App-content">
-        <p>cvespa</p>
+
       </div>
       <footer className="App-footer">
-        <SocialMediaList instagram="bpotacon" github="cvespa" minecraft="bpotacon" />
+        <SocialMediaList games={games} socials={socials}/>
       </footer>
     </div>
   );
