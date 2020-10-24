c528445033201155eb86c08e3c31594412685aa3
cvespa <christian@algomus.com>
2020-10-23 22:47:26 -0400

    Eh, finally build out a page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import SocialMediaList from './SocialMediaList.js'
-import MainContainer from './MainContainer';
 import './App.css';
 
 library.add(fab);
@@ -12,18 +10,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-
-        <MainContainer />
-
-        <SocialMediaList instagram="bpotacon" github="cvespa" />
+        <p>Battle Programmer Otacon</p>
       </header>
+      <div className="App-content">
+        <p>cvespa</p>
+      </div>
+      <footer className="App-footer">
+        <SocialMediaList instagram="bpotacon" github="cvespa" minecraft="bpotacon" />
+      </footer>
     </div>
   );
 }
